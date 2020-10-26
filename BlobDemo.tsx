42480a3945db52d75bd102f99ea29d7c1be412e1
Christof Petig <christof.petig@arcor.de>
2020-10-26 20:12:05 +0100

    Blob tileset + test

---
+++ BlobDemo.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Blob demo" tilewidth="32" tileheight="32" tilecount="15" columns="0" canrotate="1">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="0.gif"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="1.gif"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="5.gif"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="7.gif"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="17.gif"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="21.gif"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="23.gif"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="29.gif"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="31.gif"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="85.gif"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="87.gif"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="95.gif"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="119.gif"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="127.gif"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="255.gif"/>
+ </tile>
+ <wangsets>
+  <wangset name="New Wang Set" type="mixed" tile="-1">
+   <wangcolor name="water" color="#0055ff" tile="-1" probability="1"/>
+   <wangcolor name="ground" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="1" wangid="2,1,1,1,1,1,1,1"/>
+   <wangtile tileid="2" wangid="2,1,2,1,1,1,1,1"/>
+   <wangtile tileid="3" wangid="2,2,2,1,1,1,1,1"/>
+   <wangtile tileid="4" wangid="2,1,1,1,2,1,1,1"/>
+   <wangtile tileid="5" wangid="2,1,2,1,2,1,1,1"/>
+   <wangtile tileid="6" wangid="2,2,2,1,2,1,1,1"/>
+   <wangtile tileid="7" wangid="2,1,2,2,2,1,1,1"/>
+   <wangtile tileid="8" wangid="2,2,2,2,2,1,1,1"/>
+   <wangtile tileid="9" wangid="2,1,2,1,2,1,2,1"/>
+   <wangtile tileid="10" wangid="2,2,2,1,2,1,2,1"/>
+   <wangtile tileid="11" wangid="2,2,2,2,2,1,2,1"/>
+   <wangtile tileid="12" wangid="2,2,2,1,2,2,2,1"/>
+   <wangtile tileid="13" wangid="2,2,2,2,2,2,2,1"/>
+   <wangtile tileid="14" wangid="2,2,2,2,2,2,2,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
